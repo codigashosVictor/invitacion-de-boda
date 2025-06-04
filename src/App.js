f77da76bbda99492ed348f470f03937f5605fffb
Victor Gutierrez (Code) <20462203+codigashosVictor@users.noreply.github.com>
2025-06-03 19:51:18 -0700

    changes route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Invitacion from "./Invitacion";
 // App.js
 export default function App() {
   return (
-    <Router>
+    <HashRouter basename="/invitacion-de-boda">
       <Routes>
         <Route path="/invitacion-de-boda/:family" element={<Invitacion />} />
-        {/* otras rutas si necesitas */}
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
